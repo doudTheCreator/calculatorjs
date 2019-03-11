05909233c65dbfa9b61e00e42315f4493f8b5f72
Sean Carroll <Seancarroll165@gmail.com>
2019-03-11 20:50:50 +0000

    tidied it up abit

--- index.js
+++ index.js
@@ -1,51 +1,23 @@
 var qb = 0
 var wb = 0
-var eb = 0
+var eb = 0 
 var rb = false // premier nombre
 var tb = 0 // Type: 0 = rien, 1 = ajoute, 2 = soustraction
 
 function affichage(nombre)
 {
-		var b0 = document.getElementById("b0")
-		b0.innerHTML=b0.innerHTML + nombre
+		var b0 = document.getElementById("b0");
+		b0.innerHTML= b0.innerHTML + nombre
 }
-function force(nombre)
-{
-		var b0 = document.getElementById("b0")
-		b0.innerHTML= nombre
-}
-function reset (z)
-{
-	var b0 = document.getElementById("b0")
 
-	if(z != 0)
-	{
-		tb = z
-		if(rb == false)
-		{
-			qb = Number(b0.innerHTML)
-			rb = true
-		}
-		else
-		{
-			wb = Number(b0.innerHTML)
-		}
-	}
+function total(){
+	let doc = document.getElementById("b0");
+	b0.innerText = eval(doc.innerText);
 
-	b0.innerHTML = ""
 
 }
 
-function total()
-{
-	wb = Number(b0.innerHTML) // weird fix but ok - bink
-	reset(0)
-	if(tb==1)
-	{
-		force(qb + wb)
-	}
-	else if (tb==2)
-	{
-		force (qb - wb)
-  }
+function addOps(o){
+	affichage(o)
 }
+
