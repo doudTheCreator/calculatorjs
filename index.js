c509c320f16160f4d81852cf2a8c09c143721703
doudTheCreator <edouard.cossette@gmail.com>
2019-03-11 13:01:46 -0500

    Quasiment rien

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var wb = 0
 var eb = 0
 var rb = false // premier nombre
 var tb = 0 // Type: 0 = rien, 1 = ajoute, 2 = soustraction
+
 function affichage(nombre)
 {
 		var b0 = document.getElementById("b0")
