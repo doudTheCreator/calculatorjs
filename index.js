93f3ca068a352102b095fb4fa0af52a750b35fd2
doudTheCreator <edouard.cossette@gmail.com>
2019-03-11 12:38:21 -0500

    Ca marche1git push1jsdvnnehfghndcaneue eirddn   trhte de le si li so si kla !

--- index.js
+++ index.js
@@ -1,23 +1,44 @@
 var qb = 0
 var wb = 0
 var eb = 0
-var rb = false
+var rb = false // premier nombre
 var tb = 0 // Type: 0 = rien, 1 = ajoute, 2 = soustraction
 function affichage(nombre)
 {
 		var b0 = document.getElementById("b0")
 		b0.innerHTML=b0.innerHTML + nombre
 }
+function force(nombre)
+{
+		var b0 = document.getElementById("b0")
+		b0.innerHTML= nombre
+}
 function reset (z)
 {
 	tb=z
-	rb = true
 	var b0 = document.getElementById("b0")
+	if(rb == false)
+	{
 	qb = Number(b0.innerHTML)
+	rb = true
+	}
+
+	if(rb == true)
+	{
+	wb = Number(b0.innerHTML)
+	}
 	b0.innerHTML = ""
 	
 }
 
 function total()
 {
+	if(tb==1)
+	{
+		force(qb + wb)
+	}
+	else if (tb==2)
+	{
+		force (qb - wb)
+    }	
 }
